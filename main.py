0e9bdae52e165898e8919da017cdc4ecf0a0a026
Matt Parmett <matt.parmett@gmail.com>
2021-07-08 17:28:00 -0700

    complete assignment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import random
+import json
 
 import requests
 from flask import Flask, send_file, Response
@@ -9,20 +11,29 @@ app = Flask(__name__)
 
 def get_fact():
 
-    response = requests.get("http://unkno.com")
+    response = requests.get("http://randomfactgenerator.net")
 
     soup = BeautifulSoup(response.content, "html.parser")
-    facts = soup.find_all("div", id="content")
+    facts = soup.find_all("div", id="z")
 
-    return facts[0].getText()
+    return facts[0].getText().split("\n")[0]
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    # return "test"
+    data = {'input_text': get_fact()}
+    pig_latin_url = requests.post("https://hidden-journey-62459.herokuapp.com/piglatinize/",
+                                  data=data, 
+                                  allow_redirects=False
+                                  ).headers['location']
+    
+    return ("<html><body>Here is the link to a pig-latinized fact: "
+            f"<a href={pig_latin_url}>{pig_latin_url}</a></body></html>")
 
 
 if __name__ == "__main__":
+    print(get_fact())
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
 
